6bb02a7090d963242236549f176fe8c0ed337ec0
anacarolcortez <anacarolcortez@hotmail.com>
2022-04-05 10:25:05 -0300

    case else

--- gamersclub/src/abc/case.sql
+++ gamersclub/src/abc/case.sql
@@ -2,9 +2,10 @@ SELECT
 
     idPlayer,
     descCountry,
-    CASE WHEN descCountry = 'br' THEN 'brasileira'
+    CASE 
+        WHEN descCountry = 'br' THEN 'brasileira'
         WHEN descCountry = 'ar' THEN 'argentina'
-        WHEN descCountry = 'cl' THEN 'colombiana'
+        WHEN descCountry = 'cl' THEN 'chilena'
         WHEN descCountry = 'pe' THEN 'peruana'
         WHEN descCountry = 'uy' THEN 'uruguaia'
         WHEN descCountry = 'py' THEN 'paraguaia'
@@ -15,6 +16,19 @@ SELECT
         WHEN descCountry = 'sv' THEN 'salvadorenha'
         WHEN descCountry = 'cr' THEN 'costarriquenha'
         WHEN descCountry = 'ec' THEN 'equatoriana'
+        ELSE 'NA'
     END AS nacionalidade
 
-FROM tb_players
+FROM tb_players;
+
+SELECT
+
+    idPlayer,
+    descCountry,
+    CASE 
+        WHEN descCountry = 'br' THEN 'sim'
+        WHEN descCountry IN ('ar', 'cl', 'uy') THEN 'talvez'
+        ELSE 'não'
+    END AS promocao
+
+FROM tb_players;
